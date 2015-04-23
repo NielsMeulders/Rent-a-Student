f9920500632020368c92a3f29e8f8665ca3ad4f6
TwixSibaya <r0308266@student.thomasmore.be>
2015-04-23 12:37:12 +0200

    styling

--- registreer_bezoeker.php
+++ registreer_bezoeker.php
@@ -34,6 +34,7 @@
     <title>Rent-a-Student</title>
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/reset.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <meta name="keywords" content="Rentastudent, rent a student, imd, interactive, multimedia, design, thomas, more, mechelen, app">
     <meta name="description" content="Zin om een dagje mee te volgen met een IMD-student? Boek hier je persoonlijke IMD-gids!" />
@@ -41,27 +42,47 @@
 </head>
 <body>
 
-    <?php if (!isset($_SESSION['FBID'])): ?>
+    <?php if (isset($_SESSION['FBID'])): ?>
         <?php echo $_SESSION['EMAIL']; ?>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12 no-padding">
+            <div id="topbar"></div>
+            <div class="jumbotron register" id="jumbologin"><h1>Registreer via Facebook</h1></div> <!--end jumbotron-->
+            <form id="loginform" action="" method="post">
+                 <div class="form-group">
+                    <img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture" alt="Pic"/>
+                </div>
+                <div class="form-group">
+                    <label for="name">Naam</label>
+                    <input type="text" class="form-control" name="name" id="name" <?php echo " value='". $_SESSION['FULLNAME']."'"; ?>/>
+                </div>
+                <div class="form-group">
+                    <label for="email" >Email</label>
+                    <input type="email" class="form-control" name="email" id="email"/>
+                </div>
+                
 
-        <form action="" method="post">
-            <label for="name">Naam</label>
-            <input type="text" name="name" id="name" <?php echo " value='". $_SESSION['FULLNAME']."'"; ?>/>
 
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email"/>
+                <button id="post_btn" class="btn btn-default">Registreer!</button>
+                <div><a href="logout.php">Logout</a></div>
+            </form>
+        </div><!--end column-->
+    </div><!--end row-->
 
-            <label for="pic">Profielfoto</label>
-            <img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture" alt="Pic"/>
-
-            <button id="post_btn">Registreer!</button>
-            <div><a href="logout.php">Logout</a></div>
-        </form>
-
+    <div class="row" >        
+    <div class="col-xs-12" id="relatedlinks">
+        <ul>
+           <li><a href="http://www.thecreativitygym.be/"><img src="img/gym_logo.png" alt="Creativity Gym logo"></a></li>
+           <li><a id="weareimd" href="http://www.weareimd.be/"><img src="img/weareimd_logo.svg" alt="We Are IMD logo"></a></li>
+           <li><a id="thomasmore" href="http://www.thomasmore.be/interactive-multimedia-design-imd"><img src="img/thomasmore_logo.png" alt="Thomas More logo"></a></li>
+        </ul>
+    </div><!--end relatedlinks-->
+    </div><!--end row-->
     <?php else: ?>
         <div class="container">
-            <a href="fbconfig.php">Login with Facebook</a>
-        </div>
+            <a href="fbconfig.php" id="loginwithfb">Login with Facebook</a>
+    </div>
     <?php endif ?>
 
     <?PHP
@@ -71,6 +92,7 @@
 
     }*/
     ?>
-
+    <footer class="footer">&copy Thomas More</footer>
+    </div><!--end container-->
 </body>
 </html>
