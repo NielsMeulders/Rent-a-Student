a0957b1f141d073dc1b1fc765c2ff922313d5dbd
NielsMeulders <niels.meulders@telenet.be>
2015-04-30 00:10:18 +0200

    visitor permissions

--- bezoeker_home.php
+++ bezoeker_home.php
@@ -1,3 +1,9 @@
+<?PHP
+
+    session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +22,8 @@
 </head>
 
 <body>
+
+<?PHP if (!empty($_SESSION['loggedIn']) && $_SESSION['type']=='bezoeker'): ?>
 <div class="container-fluid" >
 
 <!--nav-->
@@ -81,6 +89,55 @@
     </div><!--end row-->
     <footer class="footer">&copy Thomas More</footer>
 
+<?PHP else: ?>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Rent-a-Student</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a class="btn" id="btnlogout" href="logout.php">Logout</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav><!--end nav-->
+
+    <div class="col-md-12" id="bezhometitle">
+        <h1>Oeps!</h1>
+    </div>
+
+    <div class="row rowhomepage">
+        <div id="welcomewrap">
+            <div id="collinks"class="col-xs-6 text-left">
+                <div class="jumbotron">
+                    <h3>U heeft geen toegang tot deze pagina!</h3>
+                    <p>Sorry,</p>
+                    <p>om deze pagina te bekijken moet u ingelogd zijn als bezoeker.
+                    Als u <a href="index.php">hier</a> klikt, zorgen wij ervoor dat u op de juist pagina terecht komt.</p>
+                    <p>Bedankt.</p>
+                </div>
+            </div><!--end collinks-->
+        </div><!--end welcomewrap-->
+    </div><!-- end row -->
+
+    <div class="row" >
+        <div class="col-md-12" id="relatedlinks">
+            <ul>
+                <li><a href="http://www.thecreativitygym.be/"><img src="img/gym_logo.png" alt="Creativity Gym logo"></a></li>
+                <li><a id="weareimd" href="http://www.weareimd.be/"><img src="img/weareimd_logo.svg" alt="We Are IMD logo"></a></li>
+                <li><a id="thomasmore" href="http://www.thomasmore.be/interactive-multimedia-design-imd"><img src="img/thomasmore_logo.png" alt="Thomas More logo"></a></li>
+            </ul>
+        </div><!--end relatedlinks-->
+    </div><!--end row-->
+    <footer class="footer">&copy Thomas More</footer>
+<?PHP endif; ?>
 
 </div> <!--end container-->
 </body>
